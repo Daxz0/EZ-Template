694bdc045971d9de4936de9466ed966593e00368
William Sellery <nightwolf3140@gmail.com>
2025-10-02 17:59:05 -0400

    Fixed changes made in testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
     {-5, -6, -7, -8},  // Left Chassis Ports (negative port will reverse it!)
-    {11, 15, 1, 17},  // Right Chassis Ports (negative port will reverse it!)
+    {11, 15, 16, 17},  // Right Chassis Ports (negative port will reverse it!)
 
     {20, 21},  // IMU Port
     4.125,     // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
